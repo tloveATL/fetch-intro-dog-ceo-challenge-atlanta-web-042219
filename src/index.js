3a7d32ac15e298d2468c480cb146a6ce492d21ee
tloveATL <tlove.weaver@gmail.com>
2019-06-12 15:18:38 -0400

    all functions except filter by breed

--- src/index.js
+++ src/index.js
@@ -1,1 +1,58 @@
-console.log('%c HI', 'color: firebrick')
+document.addEventListener('DOMContentLoaded', function (){
+
+    console.log('%c HI', 'color: firebrick')
+    // document.querySelector('#breed-dropdown').addEventListener('select', console.log("um are you working or nah?", fetchImages()))
+fetch('https://dog.ceo/api/breeds/image/random/4')
+    .then(res => res.json())
+    .then(res => {
+        let images = res.message
+        images.forEach(image => addNewImage(image))
+})
+
+
+      // .then((data)=> addImages(data))
+      // .catch((error)=>console.log("It didn't work."))
+
+fetch('https://dog.ceo/api/breeds/list/all')
+    .then(res => res.json())
+    .then(res => {
+        let breeds = res.message
+        for (let [key, value] of Object.entries(breeds)) {
+    addNewBreed(key)};
+    })
+
+
+
+})
+
+  // let images = imageArray()
+
+function addImages(){
+    imageArray.forEach(img => {addNewImage(img)})
+}
+
+
+function addNewImage(image){
+    let dogContainer = document.querySelector('#dog-image-container')
+
+    const img = document.createElement("img")
+    img.src = image
+
+    dogContainer.appendChild(img)
+
+}
+
+function addNewBreed(key){
+    let breedList = document.querySelector('#dog-breeds')
+    const li = document.createElement('li')
+    li.innerHTML = key
+
+    breedList.appendChild(li)
+
+    li.addEventListener('click', changeColor, false)
+}
+
+function changeColor(e) {
+    console.log(e.target)
+    e.target.style = "color:orange"
+}
